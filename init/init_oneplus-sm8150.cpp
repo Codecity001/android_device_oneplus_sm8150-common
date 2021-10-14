709c7a36e614fbb019a634e7c9f7e7f68f65f15a
Codecity001 <sanganiharish263@gmail.com>
2021-10-14 12:05:08 +0530

    sm8150-common: init: Not needed to override anymore

--- init/init_oneplus-sm8150.cpp
+++ init/init_oneplus-sm8150.cpp
@@ -50,13 +50,6 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void property_override_multi(char const system_prop[], char const vendor_prop[], char const bootimage_prop[], char const value[])
-{
-  property_override(system_prop, value);
-  property_override(vendor_prop, value);
-  property_override(bootimage_prop, value);
-}
-
 void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[]) {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
@@ -226,6 +219,4 @@ void vendor_load_properties() {
 
   // dalvikvm props
   load_dalvikvm_properties();
-  // fingerprint
-  property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint", "ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys");
 }
